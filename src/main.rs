a5aa253c50e17f236494af4b49f48081908e4851
__metamethods <tabledmetamethods@gmail.com>
2025-08-17 11:18:50 -0700

    refactor: bind to 0.0.0.0 instead of 127.0.0.1

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ async fn main() -> std::io::Result<()> {
                     .configure(routes::publish::config),
             )
     })
-    .bind(("127.0.0.1", port))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
